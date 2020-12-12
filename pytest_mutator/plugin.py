a80f7aa438314528a3e4e74c0f5a40c27178bcdf
Nathan Page <nathanrpage97@gmail.com>
2020-12-12 13:42:59 -0800

    add in the start of additional functionality

---
+++ pytest_mutator/plugin.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+from typing import Optional
+
+from . import Context
+from . import mutate
+from .cache import Mutant
+from .cache import filename_and_mutation_id_from_pk
+from .cache import update_line_numbers
+from .loader import install
+
+
+if TYPE_CHECKING:
+    from _pytest.config import Config
+    from _pytest.config import PytestPluginManager
+    from _pytest.config.argparsing import Parser
+
+
+def pytest_configure(config: "Config") -> None:
+    mutant_id: Optional[int] = config.getoption("mutant_id", default=None)
+
+    if mutant_id is None:
+        return
+
+    install(mutant_id)
+
+
+def pytest_addoption(parser: "Parser", pluginmanager: "PytestPluginManager") -> None:
+    parser.addoption("--mutant-id", dest="mutant_id", type=int)
